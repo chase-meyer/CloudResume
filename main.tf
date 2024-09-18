eeead3a2c3e4e257f8923e5655407cf11b477120
Chase Meyer <chase.e.meyer@gmail.com>
2024-09-18 07:44:52 -0500

    add cdn with custom domain

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 4.0.1"
     }
   }
 
@@ -27,6 +27,11 @@ resource "azurerm_resource_group" "prod" {
   location = "centralus"
 }
 
+resource "azurerm_resource_group" "dom" {
+  name     = "domain-resources"
+  location = "centralus"
+}
+
 resource "azurerm_storage_account" "prod" {
   name                     = var.storage_acct
   resource_group_name      = azurerm_resource_group.prod.name
@@ -41,7 +46,45 @@ resource "azurerm_storage_account" "prod" {
   }
 
   network_rules {
-    default_action = "Deny"
-    ip_rules       = [var.my_ip]
+    default_action = "Allow"
   }
+}
+
+resource "azurerm_cdn_profile" "prod" {
+  name                = "resume-cdn-profile"
+  location            = azurerm_resource_group.prod.location
+  resource_group_name = azurerm_resource_group.prod.name
+  sku                 = "Standard_Verizon"
+}
+
+resource "azurerm_cdn_endpoint" "prod" {
+  name                = "resume-endpoint"
+  profile_name        = azurerm_cdn_profile.prod.name
+  location            = azurerm_resource_group.prod.location
+  resource_group_name = azurerm_resource_group.prod.name
+
+  origin {
+    name      = "prod"
+    host_name = azurerm_storage_account.prod.primary_web_host
+  }
+}
+
+resource "azurerm_dns_zone" "prod" {
+  name                = "chase-meyer.space"
+  resource_group_name = azurerm_resource_group.dom.name
+}
+
+resource "azurerm_dns_cname_record" "prod" {
+  name                = "resume"
+  zone_name           = resource.azurerm_dns_zone.prod.name
+  resource_group_name = resource.azurerm_dns_zone.prod.resource_group_name
+  ttl                 = 3600
+  target_resource_id  = azurerm_cdn_endpoint.prod.id
+}
+
+resource "azurerm_cdn_endpoint_custom_domain" "prod" {
+  name            = "resume-domain"
+  cdn_endpoint_id = azurerm_cdn_endpoint.prod.id
+  host_name       = "${azurerm_dns_cname_record.prod.name}.${resource.azurerm_dns_zone.prod.name}"
+  cdn_managed_https
 }
