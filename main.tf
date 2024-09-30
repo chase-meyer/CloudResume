ba58318792c4516c6cdc5f72d8949bcc5b07cb50
Chase Meyer <chase.e.meyer@gmail.com>
2024-09-30 18:00:55 -0500

    Add cosmosdb for table

--- main.tf
+++ main.tf
@@ -22,16 +22,19 @@ provider "azurerm" {
   }
 }
 
+# add resource group for resume objects
 resource "azurerm_resource_group" "prod" {
   name     = "resume-resources"
   location = "centralus"
 }
 
+# add resource group for domain
 resource "azurerm_resource_group" "dom" {
   name     = "domain-resources"
   location = "centralus"
 }
 
+# storage account and static website for resueme
 resource "azurerm_storage_account" "prod" {
   name                     = var.storage_acct
   resource_group_name      = azurerm_resource_group.prod.name
@@ -50,6 +53,7 @@ resource "azurerm_storage_account" "prod" {
   }
 }
 
+# cdn profile for resume using Standard_Microsoft tier
 resource "azurerm_cdn_profile" "prod" {
   name                = "resume-cdn-profile"
   location            = azurerm_resource_group.prod.location
@@ -57,6 +61,7 @@ resource "azurerm_cdn_profile" "prod" {
   sku                 = "Standard_Microsoft"
 }
 
+# connect cdn to static website on the storage account
 resource "azurerm_cdn_endpoint" "prod" {
   name                = "resume-endpoint"
   profile_name        = azurerm_cdn_profile.prod.name
@@ -72,11 +77,13 @@ resource "azurerm_cdn_endpoint" "prod" {
   origin_host_header = azurerm_storage_account.prod.primary_web_host
 }
 
+# dns zone for my domain
 resource "azurerm_dns_zone" "prod" {
   name                = "chase-meyer.space"
   resource_group_name = azurerm_resource_group.dom.name
 }
 
+# cname record resume.chase-meyer.space pointing to cdn endpoint
 resource "azurerm_dns_cname_record" "prod" {
   name                = "resume"
   zone_name           = resource.azurerm_dns_zone.prod.name
@@ -85,6 +92,7 @@ resource "azurerm_dns_cname_record" "prod" {
   target_resource_id  = azurerm_cdn_endpoint.prod.id
 }
 
+# assign cdn a custom domain and give it a managed https so azure auto supplies ssl cert
 resource "azurerm_cdn_endpoint_custom_domain" "prod" {
   name            = "resume-domain"
   cdn_endpoint_id = azurerm_cdn_endpoint.prod.id
@@ -95,4 +103,43 @@ resource "azurerm_cdn_endpoint_custom_domain" "prod" {
     protocol_type    = "ServerNameIndication"
     tls_version      = "TLS12"
   }
+}
+
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_cosmosdb_account" "prod" {
+  name                = "resume-cosmos-db-${random_integer.ri.result}"
+  location            = azurerm_resource_group.prod.location
+  resource_group_name = azurerm_resource_group.prod.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+
+  capabilities {
+    name = "EnableTable"
+  }
+
+  capabilities {
+    name = "EnableServerless"
+  }
+
+  consistency_policy {
+    consistency_level       = "Eventual"
+  }
+
+  geo_location {
+    location          = "westus3"
+    failover_priority = 0
+  }
+
+}
+
+resource "azurerm_cosmosdb_table" "prod" {
+  name                = "resume-cosmos-table"
+  resource_group_name = resource.azurerm_cosmosdb_account.prod.resource_group_name
+  account_name        = resource.azurerm_cosmosdb_account.prod.name
 }
