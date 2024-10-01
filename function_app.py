4c64323854b26a19e9dc42d846f8e675955a6318
Chase Meyer <chase.e.meyer@gmail.com>
2024-10-01 12:43:47 -0500

    Setup Azure Functions Python project layout

---
+++ function_app.py
@@ -0,0 +1,10 @@
+import azure.functions as func
+
+app = func.FunctionApp()
+
+  throughput          = 400
+@app.function_name(name="HttpTrigger1")
+@app.route(route="req")
+def main(req: func.HttpRequest) -> str:
+    user = req.params.get("user")
+    return f"Hello, {user}!"
