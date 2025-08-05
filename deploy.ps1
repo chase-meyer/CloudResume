a73f546d49bb7c034fc65324379d874496fd543c
Chase Meyer <chase.e.meyer@gmail.com>
2025-08-05 20:41:12 +0000

    change deploy.sh to powershell deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,8 @@
-#!/bin/bash
-
 # Deploy resource groups
 az deployment sub create --location centralus --template-file main.bicep
 
 # Deploy resume resources
-az deployment group create --resource-group resume-resources --template-file resume-rg.bicep --parameters @resume-params.json
+az deployment group create --resource-group resume-resources --template-file resume-rg.bicep --parameters "@resume-params.json"
 
 # Deploy domain resources
 az deployment group create --resource-group domain-resources --template-file domain-rg.bicep --parameters dnsZoneName=chase-meyer.space
